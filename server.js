044598d9e7fbf38663957b45064e1f95394bd80a
abisgit <abeselomlh@gmail.com>
2025-04-21 15:28:20 -0700

    server.js cors  updated

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const jwt = require('jsonwebtoken');
 
 // Add CORS headers
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  // res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   next();
